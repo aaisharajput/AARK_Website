57426bcccaad9e1213c3e988f30fd22b52fbd6d9
aaisharajput <109970085+aaisharajput@users.noreply.github.com>
2024-01-19 14:46:07 +0530

    Update server.js

--- AARK Server/server.js
+++ AARK Server/server.js
@@ -1,24 +1,49 @@
+import dotenv from 'dotenv';
+dotenv.config({path:'./config.env'}); //set path
+
 import express from "express";
-import session from "express-session";
 import cors from 'cors';
-import {init} from './Models/db.js';
-// import {IndexRoutes} from "./Routes/IndexRoutes.js";
-// import cookieParser from "cookie-parser";
+import {IndexRoutes} from "./Routes/IndexRoutes.js";
 
 const app = express();
-const port = 3000;
+const port =process.env.PORT || 3000;
+
 app.use(express.static('./media'));
 app.use(express.static('./'));
-app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.json());
-app.use(session({secret:"AarkGlobal@12#3",resave:false,saveUninitialized:true,}));
+app.use(express.urlencoded({extended:true}))
+// app.use(cors());
 
-// app.use('/User',IndexRoutes);
+const allowedOrigins = ['http://localhost:5173'];
+const corsOptions = {
+    origin: function (origin, callback) {
+        // Check if the origin is allowed or if it's a same-origin request (null)
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+    methods: 'POST',
+    credentials: true, // enable passing credentials (cookies, HTTP authentication) across domains
+  };
 
-init().then((result)=>{
-    console.log(result)
-    app.listen(port,()=>{console.log("port no.: ",port);});
-}).catch(err=>console.log(err))
+  // Custom CORS error handling middleware
+const handleCorsError = (err, req, res, next) => {
+    if (err.message === 'Not allowed by CORS') {
+        console.log("cors")
+      res.status(400).json({Response:"",Error:"Not allowed by CORS",Status:400});
+    } else {
+      next(err);
+    }
+  };
 
+app.use(cors(corsOptions));
+app.use(handleCorsError);
+
+app.use('/User',IndexRoutes);
+
+// console.log(app.get('env'))
+// console.log(process.env.PORT)
+
+app.listen(port,()=>{console.log("port no.: ",port);});
