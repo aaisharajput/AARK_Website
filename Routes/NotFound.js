b8a9658c5055195d3a65017fdd6d2bbf3c3241ef
aaisharajput <sapnadevi@aarkglobalinc.com>
2024-01-22 11:02:48 +0530

    moo

--- Routes/NotFound.js
+++ Routes/NotFound.js
@@ -1,10 +1,14 @@
-import express from "express";
-import path from "path";
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const Router = express.Router();
 
-Router.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"client","build","index.html"))
-})
+Router.get('*', (req, res) => {
+  res.sendFile(resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 export { Router as NOTFOUND };
